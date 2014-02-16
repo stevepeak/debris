d8d35bcb194b40b42a51f48ac10ba81e6d25c0ec
stevepeak <steve@stevepeak.net>
2014-02-16 18:41:52 -0500

    Resevoir for cache (conceptual)

--- debris/__init__.py
+++ debris/__init__.py
@@ -2,6 +2,7 @@ version = VERSION = __version__ = '0.0.2'
 
 from . import locale
 from .object import Object
+from .reservoir import Reservoir
 
 try:
     import tornado.web
---
+++ debris/reservoir.py
@@ -0,0 +1,12 @@
+class Reservoir(object):
+    def clear(self, what):
+        pass
+
+    def get(self, index):
+        pass
+
+    def set(self, index, value):
+        pass
+
+    def all(self):
+        pass
