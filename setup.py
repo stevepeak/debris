d66f1807f20a1a3cf97e3a08dd34d57dd60b979a
stevepeak <steve@stevepeak.net>
2014-02-17 07:19:19 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,21 @@ from setuptools import setup
 
 version = '0.0.2'
 
+classifiers = ["Development Status :: 1 - Planning",
+               "License :: OSI Approved :: Apache Software License",
+               "Programming Language :: Python",
+               "Programming Language :: Python :: 2.7",
+               "Programming Language :: Python :: 3.2",
+               "Programming Language :: Python :: 3.3",
+               "Programming Language :: Python :: Implementation :: PyPy"]
+
 setup(name='debris',
       version=version,
-      description="Recycle your objects",
-      long_description="""Easy methods to manage objects to reduce stress on
-                          retreivals.""",
-      classifiers=["Development Status :: 1 - Planning",
-                   "License :: OSI Approved :: Apache Software License",
-                   "Programming Language :: Python",
-                   "Programming Language :: Python :: 2.7",
-                   "Programming Language :: Python :: 3.2",
-                   "Programming Language :: Python :: 3.3",
-                   "Programming Language :: Python :: Implementation :: PyPy"],
-      keywords="cache tornado",
+      description="Cache objects, web requests and page builds",
+      long_description="""Methods to manange data effectively through 
+                          asset controls.""",
+      classifiers=classifiers,
+      keywords="cache tornado memcache memory ram mongodb redis assets",
       author='@stevepeak',
       author_email='steve@stevepeak.net',
       url='http://github.com/stevepeak/debris',
