cf0b9b1006e324aa88bd3d6768482b78864a9d4a
stevepeak <steve@stevepeak.net>
2014-02-18 16:20:02 -0500

    added debug mode for rendering templates

--- debris/wrappers/tornado.py
+++ debris/wrappers/tornado.py
@@ -16,7 +16,7 @@ def _replace_finish(handler, namespace, storage):
         handler.finish = _rp
     handler.finish = finish
 
-def request(namespace=None, storage=None):
+def request(namespace=None, storage=None, debug=False):
     """
     Wrapper for tornado requests. Example
 
@@ -32,20 +32,22 @@ def request(namespace=None, storage=None):
     def wrapper(_f):
         @functools.wraps(_f)
         def _stash(self, *a, **k):
-            _namespace = helpers.call(namespace)
-            if _namespace:
-                _storage = helpers.call(storage, self, namespace) or debris.storage.memory
-                # this request is cacheable
-                if _storage:
-                    data = _storage.get(namespace)
-                    # return the cache result
-                    if data:
-                        self.finish(data)
-                    else:
-                        _replace_finish(self, namespace, _storage)
-                        # get the result of this request
-                        _f(self, *a, **k)
-                    return
+            if debug is False:
+                # easy way to skip the stash
+                _namespace = helpers.call(namespace)
+                if _namespace:
+                    _storage = helpers.call(storage, self, namespace) or debris.storage.memory
+                    # this request is cacheable
+                    if _storage:
+                        data = _storage.get(namespace)
+                        # return the cache result
+                        if data:
+                            self.finish(data)
+                        else:
+                            _replace_finish(self, namespace, _storage)
+                            # get the result of this request
+                            _f(self, *a, **k)
+                        return
             # request is not cacheable
             _f(self, *a, **k)
         return _stash
